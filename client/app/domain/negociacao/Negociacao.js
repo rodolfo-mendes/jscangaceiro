11bcab374beb8a381ac4c271b89a62713d1473ef
Rodolfo Mendes <rodolfo@rsvarejo.com.br>
2018-02-01 14:00:35 -0200

    Commit ch3

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,23 @@
+class Negociacao{
+    constructor(_data, _quantidade, _valor){
+        Object.assign(this, {_quantidade, _valor});
+        this._data = new Date(_data.getTime());
+        Object.freeze(this);
+    }
+
+    get volume(){
+        return this._quantidade * this._valor;
+    }
+
+    get data(){
+        return this._data;
+    }
+
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+}
