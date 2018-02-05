219675ba59ab2f1e191306014c15c2e3597678a4
Rodolfo Mendes <rodolfo@rsvarejo.com.br>
2018-02-05 12:51:28 -0200

    ch09

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,15 +1,71 @@
-class NegociacaoController{
-    adiciona(event){
+class NegociacaoController {
+
+    constructor() {
+
+        const $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+        const self = this;
+        this._negociacoes = new Proxy(new Negociacoes(), {
+            
+            get(target, prop, receiver) {
+            
+                if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia']
+                    .includes(prop)) {
+                        
+                        return function() {
+
+                            console.log(`"${prop}" disparou a armadilha`);
+                            target[prop].apply(target, arguments);
+                            self._negociacoesView.update(target);
+                        }
+
+                } else {
+
+                    return target[prop];
+                }
+            }
+        });
+
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        this._negociacoesView.update(this._negociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView('#mensagemView');
+        this._mensagemView.update(this._mensagem);
+    }
+    adiciona(event) {
+
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
+        this._limpaFormulario();
+    }
 
-        let $ = document.querySelector.bind(document);
+    _limpaFormulario() {
 
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
+    }
 
-        console.log(inputData.value);
-        console.log(parseInt(inputQuantidade.value));
-        console.log(parseFloat(inputValor.value));
+    _criaNegociacao() {
+
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+    }
+
+    apaga() {
+
+        this._negociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
     }
 }
--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,23 +1,29 @@
-class Negociacao{
-    constructor(_data, _quantidade, _valor){
-        Object.assign(this, {_quantidade, _valor});
+class Negociacao {
+
+    constructor(_data, _quantidade, _valor) {
+        
+        Object.assign(this, { _quantidade, _valor })
         this._data = new Date(_data.getTime());
         Object.freeze(this);
     }
 
-    get volume(){
+    get volume() {
+
         return this._quantidade * this._valor;
     }
 
-    get data(){
-        return this._data;
+    get data() {
+
+        return new Date(this._data.getTime());
     }
 
-    get quantidade(){
+    get quantidade() {
+
         return this._quantidade;
     }
 
-    get valor(){
+    get valor() {
+
         return this._valor;
     }
 }
