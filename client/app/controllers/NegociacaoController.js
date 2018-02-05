c2abcbc02f516dcd375524963edbf84bd322ddbd
Rodolfo Mendes <rodolfo@rsvarejo.com.br>
2018-02-05 13:24:56 -0200

    Capitulo 10

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,52 +1,33 @@
 class NegociacaoController {
 
     constructor() {
-
         const $ = document.querySelector.bind(document);
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        
+        this._negociacoes = new Bind(
+            new Negociacoes(),
+            new NegociacoesView('#negociacoes'),
+            'adiciona', 'esvazia'
+        );
 
-        const self = this;
-        this._negociacoes = new Proxy(new Negociacoes(), {
-            
-            get(target, prop, receiver) {
-            
-                if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia']
-                    .includes(prop)) {
-                        
-                        return function() {
-
-                            console.log(`"${prop}" disparou a armadilha`);
-                            target[prop].apply(target, arguments);
-                            self._negociacoesView.update(target);
-                        }
-
-                } else {
-
-                    return target[prop];
-                }
-            }
-        });
-
-        this._negociacoesView = new NegociacoesView('#negociacoes');
-        this._negociacoesView.update(this._negociacoes);
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView('#mensagemView');
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView('#mensagemView'),
+            'texto'
+        );
     }
-    adiciona(event) {
 
+    adiciona(event) {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
     _limpaFormulario() {
-
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0
@@ -63,9 +44,7 @@ class NegociacaoController {
     }
 
     apaga() {
-
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
 }
